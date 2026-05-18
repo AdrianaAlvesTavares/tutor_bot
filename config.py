c4eceaba9bc6813641f18cf21cf9044294d14720
Sabrina Frazão <112819243+sabrinafrazao@users.noreply.github.com>
2026-05-17 23:09:12 -0400

    Adicionando palavras inadequadas
    Added a comprehensive list of blacklist words to filter inappropriate content.

--- config.py
+++ config.py
@@ -8,8 +8,94 @@ GEMINI_KEY = os.getenv("GEMINI_KEY")
 BOT_TOPIC      = os.getenv("BOT_TOPIC", "Python Básico")
 
 BLACKLIST_WORDS = [
-    "palavra_ruim_1",
-    "palavra_ruim_2",
+
+    # Palavrões
+    "porra",
+    "caralho",
+    "merda",
+    "puta",
+    "puto",
+    "bosta",
+    "cacete",
+    "desgraça",
+    "fdp",
+    "filho da puta",
+    "arrombado",
+    "vsf",
+    "vtnc",
+    "vai tomar no cu",
+    "vai se ferrar",
+
+    # Ofensas
+    "idiota",
+    "burro",
+    "imbecil",
+    "retardado",
+    "otário",
+    "otario",
+    "babaca",
+    "lixo",
+    "anta",
+    "nojento",
+    "escroto",
+    "ridículo",
+    "ridiculo",
+    "palhaço",
+    "palhaco",
+    "inútil",
+    "inutil",
+
+    # Bullying / preconceito
+    "racista",
+    "nazista",
+    "homofóbico",
+    "homofobico",
+    "preconceituoso",
+    "racismo",
+    "homofobia",
+
+    # Conteúdo impróprio
+    "pornografia",
+    "sexo",
+    "nudez",
+    "nudes",
+    "erótico",
+    "erotico",
+    "conteúdo adulto",
+    "conteudo adulto",
+
+    # Violência
+    "matar",
+    "assassinar",
+    "espancar",
+    "ameaça",
+    "arma",
+    "explosivo",
+
+    # Drogas
+    "maconha",
+    "cocaína",
+    "cocaina",
+    "crack",
+    "heroína",
+    "heroina",
+    "tráfico",
+    "trafico",
+
+    # Spam / golpes
+    "hack",
+    "phishing",
+    "clique aqui",
+    "ganhe dinheiro",
+    "link suspeito",
+
+    # Inglês
+    "fuck",
+    "shit",
+    "bitch",
+    "asshole",
+    "bastard",
+    "motherfucker"
 ]
 
 MAX_WARNS = 3
